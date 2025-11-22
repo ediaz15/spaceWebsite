aeadce2ac7257dac5145f0cbe2cd7b7cb424eee3
Erick Diaz <ediazluc@gmail.com>
2025-11-22 14:07:41 -0600

    Adjusted Json data structure to have an array of planets rather than multiple objects mapped to planets, used reference to render the planet title Mercury to dom!

--- assets/scripts/loadTextTest.js
+++ assets/scripts/loadTextTest.js
@@ -14,7 +14,15 @@ const loadPlanetData = file => {
     //have the data turn into json then log it
     planetData.then(response => response.json())
     .then(data => {
-        //Log it to check if it works!
+        //now we handle the data -> create elements -> add them to text nodes -> render to dom
+        const planetTitle= data.planets[0].name; //this should access the array of planets where we need to specify which ones using indexes!
+        const planetDataContainer = document.createElement("h1");
+        const planetDataText = document.createTextNode(planetTitle);
+        //we add the text to the container
+        planetDataContainer.appendChild(planetDataText);
+        //send it to the dom body
+        document.body.appendChild(planetDataContainer);
+        //log to console for testing
         console.log(data);
     })
 };
